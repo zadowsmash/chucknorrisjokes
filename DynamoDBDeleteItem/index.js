e31cc78bb1715fa3b82830425bfbf1d4c6e64c2d
Ash Rohde <ashrohde@gmail.com>
2019-03-25 20:34:32 +1100

    Updated .gitignore

--- DynamoDBDeleteItem/index.js
+++ DynamoDBDeleteItem/index.js
@@ -5,5 +5,19 @@ const { TableName, hashKey } = process.env;
 
 exports.handler = (event) => {
 
+    var params = {
+        TableName : TableName,
+        Key: {
+          HashKey: HashKey,
+          NumberRangeKey: 1
+        },
+        ConditionExpression: 'email@toremove.com',
+      };
+      
+      docClient.delete(params, function(err, data) {
+        if (err) console.log(err);
+        else console.log(data);
+      });
+
 
 };
