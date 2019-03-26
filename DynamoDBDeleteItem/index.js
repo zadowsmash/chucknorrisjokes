9014208578e4180c00ae0b4442087ef60f7d2d06
Ash Rohde <ashrohde@gmail.com>
2019-03-27 00:58:03 +1100

    DeleteItem Working as expected

--- DynamoDBDeleteItem/index.js
+++ DynamoDBDeleteItem/index.js
@@ -24,6 +24,25 @@ exports.handler = (event) => {
       console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2)); // eslint-disable-line
     } else {
       console.log("GetItem succeeded:", JSON.stringify(data, null, 2)); // eslint-disable-line
+      const finalEmailDelete = data.Items[0].email;
+      const finalUserId = data.Items[0].USERID
+
+      var params = {
+        TableName: TableName,
+        Key: {
+
+          "USERID": finalUserId,
+          "email": finalEmailDelete
+        }
+      };
+
+      docClient.delete(params, function (err, data) {
+        if (err) {
+          console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+          console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+        }
+      });
     }
   });
 
