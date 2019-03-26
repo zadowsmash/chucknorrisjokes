7d991d83378b881f8f37ef6dc09585362d3b148a
Ash Rohde <ashrohde@gmail.com>
2019-03-26 14:32:18 +1100

    Updating deploy.env files

--- DynamoDBDeleteItem/index.js
+++ DynamoDBDeleteItem/index.js
@@ -1,32 +1,31 @@
 const AWS = require('aws-sdk');
+
 const docClient = new AWS.DynamoDB.DocumentClient();
-
-
-const { TableName, HashKey } = process.env;
+const { TableName, emailDelete } = process.env;
 
 exports.handler = (event) => {
-
-  var params = {
-    RequestItems: {
-      'emailSubs': [
-        {
-          DeleteRequest: {
-            Key: { USERID: 'email@toremove.com' }
-          }
-        }
-      ]
-    }
+  console.log(event); // eslint-disable-line
+  const params = {
+    TableName,
+    ProjectionExpression: '#email, USERID',
+    ExpressionAttributeNames: {
+      '#email': 'email',
+    },
   };
-  
-  docClient.batchWrite(params, function(err, data) {
+
+  docClient.scan(params, onScan);
+  function onScan(err, data) {
     if (err) {
-      console.log("Error", err);
+      console.error(JSON.stringify(err, null, 2)); // eslint-disable-line
     } else {
-      console.log("Success", data);
+      console.log(data); // eslint-disable-line
+      };
+      
+
+      if (typeof data.LastEvaluatedKey !== 'undefined') {
+        console.log('Scanning for more...'); // eslint-disable-line
+        params.ExclusiveStartKey = data.LastEvaluatedKey;
+        docClient.scan(params, onScan);
+      }
     }
-  });
-  
-
-
-
-};
+  }
