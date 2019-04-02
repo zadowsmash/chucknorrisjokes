62d71a957b63eabf5ca9dd9cc3d52944862d326d
Ash Rohde <ashrohde@gmail.com>
2019-04-02 21:17:45 +1100

    Added confirmation email once user subscribes to local testing

--- DynamoDBPutItemLocal/index.js
+++ DynamoDBPutItemLocal/index.js
@@ -6,6 +6,7 @@ const {
 } = process.env;
 
 const dynamodb = new AWS.DynamoDB();
+const ses = new AWS.SES();
 const userId = Math.floor(new Date() / 1000);
 const userIdString = userId.toString();
 
@@ -24,4 +25,43 @@ exports.handler = (event) => {
     if (err) console.log(err, err.stack); // eslint-disable-line
     else console.log(data); // eslint-disable-line
   });
+
+  const sesparams = {
+    Destination: {
+      ToAddresses: [emailaddress]
+
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: 'UTF-8',
+          Data: ` 
+<html>
+<body> 
+
+<font size="+2"> <b> Thanks for signing up to chuck-norris-random-daily-jokes.net  <b> </font>
+<br>
+<br>
+<br>
+<font> If you ever want to UN-Subscribe, please Click </font>
+<a href="chuck-norris-random-daily-jokes.net">here</a> <font>, enter you email address in the UNSUBSCRIBE field and click.</font>
+</body> 
+</html>`,
+        },
+        Text: {
+          Charset: 'UTF-8',
+          Data: '',
+        },
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: 'Daily Random Chuck Norris Jokes',
+      },
+    },
+    Source: 'ChuckNorrisJokes@chuck-norris-random-daily-jokes.net',
+  };
+  ses.sendEmail(sesparams, (seserr, sesdata) => {
+    if (seserr) console.log(seserr, seserr.stack); // eslint-disable-line
+    else console.log(sesdata); // eslint-disable-line
+});
 };
