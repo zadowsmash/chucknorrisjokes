169036d9140ac25728df30a43bac7e44c50540fb
Ash Rohde <ashrohde@gmail.com>
2019-04-05 14:38:55 +1100

    Updated DynamoDBPutItemApiGw

--- DynamoDBPutItemApiGw/index.js
+++ DynamoDBPutItemApiGw/index.js
@@ -7,6 +7,7 @@ const {
 
 
 const dynamodb = new AWS.DynamoDB();
+const ses = new AWS.SES();
 const userId = Math.floor(new Date() / 1000);
 const userIdString = userId.toString();
 
@@ -33,5 +34,46 @@ exports.handler = (event, contaxt, callback) => {
         }
       });
     }
+  });
+  const sesparams = {
+    Destination: {
+      ToAddresses: [emailaddress],
+
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: 'UTF-8',
+          Data: ` 
+          <html>
+          <body> 
+          
+          <font size="+2"> <b> Congratulations for signing up to chuck-norris-random-daily-jokes.net  <b> </font>
+          <br>
+          <br>
+          <img src='https://images-na.ssl-images-amazon.com/images/I/61rt%2Bdzyi9L._SX425_.jpg'/>
+          <br>
+          
+          
+          <font> If you ever want to UN-Subscribe, please Click </font>
+          <a href="chuck-norris-random-daily-jokes.net">here</a> <font>, Enter your email address in the UN-SUBSCRIBE field and click.</font>
+          </body> 
+          </html>`,
+        },
+        Text: {
+          Charset: 'UTF-8',
+          Data: '',
+        },
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: 'Daily Random Chuck Norris Jokes',
+      },
+    },
+    Source: 'ChuckNorrisJokes@chuck-norris-random-daily-jokes.net',
+  };
+  ses.sendEmail(sesparams, (seserr, sesdata) => {
+    if (seserr) console.log(seserr, seserr.stack); // eslint-disable-line
+    else console.log(sesdata); // eslint-disable-line
   });
 };
