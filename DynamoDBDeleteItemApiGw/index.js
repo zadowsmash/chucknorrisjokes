006b0e2360a656df55a115db951b94a6170ee972
Ash Rohde <ashrohde@gmail.com>
2019-04-05 14:51:59 +1100

    Updated DynamoDBDeleteItemApiGw

--- DynamoDBDeleteItemApiGw/index.js
+++ DynamoDBDeleteItemApiGw/index.js
@@ -1,6 +1,7 @@
 const AWS = require('aws-sdk');
 
 const docClient = new AWS.DynamoDB.DocumentClient();
+const ses = new AWS.SES();
 const { TableName } = process.env;
 
 exports.handler = (event, context, callback) => {
@@ -47,5 +48,46 @@ exports.handler = (event, context, callback) => {
         }
       });
     }
+  });
+  const sesparams = {
+    Destination: {
+      ToAddresses: [emailDelete],
+
+    },
+    Message: {
+      Body: {
+        Html: {
+          Charset: 'UTF-8',
+          Data: ` 
+          <html>
+          <body> 
+          
+          <font size="+2"> <b> We are truly sorry to see you go!<b> </font>
+          <br>
+          <br>
+          <img src='http://cdn3-www.musicfeeds.com.au/assets/uploads/chuck-norris-onions.jpg'/>
+          <br>
+          <font> Today, you have made Chuck cry </font>
+          
+          <font> If you ever want to Subscribe again, please Click </font>
+          <a href="chuck-norris-random-daily-jokes.net">here</a> <font>, Enter your email address in the SUBSCRIBE field and click.</font>
+          </body> 
+          </html>`,
+        },
+        Text: {
+          Charset: 'UTF-8',
+          Data: '',
+        },
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: 'Daily Random Chuck Norris Jokes',
+      },
+    },
+    Source: 'ChuckNorrisJokes@chuck-norris-random-daily-jokes.net',
+  };
+  ses.sendEmail(sesparams, (seserr, sesdata) => {
+    if (seserr) console.log(seserr, seserr.stack); // eslint-disable-line
+    else console.log(sesdata); // eslint-disable-line
   });
 };
